15d4d428590ae817547f296b11f1cb5142ce86ca
Dmitrij Tkacenko <dimot9@gmail.com>
2021-01-14 19:28:52 +0100

    ✨ Add d3 network settings for user

--- components/NetworkCanvas.vue
+++ components/NetworkCanvas.vue
@@ -1,63 +1,105 @@
 <template>
-  <D3Network
-    class="border border-primary-light"
-    :net-nodes="nodes"
-    :net-links="links"
-    :options="options"
-  />
+  <div
+    class="relative border border-primary-light rounded-md space-y-4 overflow-hidden"
+  >
+    <div class="px-4 py-6 border-b border-secondary flex justify-between">
+      <div>
+        <label>Node size</label>
+        <div class="flex items-center space-x-2">
+          <input
+            type="range"
+            min="10"
+            max="100"
+            v-model="netSettings.nodeSize"
+          />
+          <span>{{ netSettings.nodeSize }}</span>
+        </div>
+      </div>
+      <div>
+        <label>Font size</label>
+        <div class="flex items-center space-x-2">
+          <input
+            type="range"
+            min="10"
+            max="36"
+            v-model="netSettings.fontSize"
+          />
+          <span>{{ netSettings.fontSize }}</span>
+        </div>
+      </div>
+      <div>
+        <label>Force</label>
+        <div class="flex items-center space-x-2">
+          <input
+            type="range"
+            min="1000"
+            max="5000"
+            v-model="netSettings.force"
+          />
+          <span>{{ netSettings.force }}</span>
+        </div>
+      </div>
+    </div>
+    <D3Network
+      ref="net"
+      :net-nodes="nodes"
+      :net-links="links"
+      :options="options"
+    />
+  </div>
 </template>
 
 <script>
 import D3Network from 'vue-d3-network'
 import 'vue-d3-network/dist/vue-d3-network.css'
 
+const defaultNetSettings = {
+  force: 3000,
+  nodeSize: 24,
+  fontSize: 12,
+}
+
 export default {
   components: {
     D3Network,
   },
-  data() {
-    return {
-      nodes: [],
-      links: [],
-      options: {
-        nodeSize: 24,
-        linkWidth: 8,
-        fontSize: 12,
-        force: 1000,
+  props: {
+    nodes: {
+      type: Array,
+      default: () => {
+        return []
+      },
+    },
+    links: {
+      type: Array,
+      default: () => {
+        return []
+      },
+    },
+  },
+  mounted() {
+    this.resetSettings()
+  },
+  computed: {
+    options() {
+      return {
+        nodeSize: this.netSettings.nodeSize,
+        fontSize: this.netSettings.fontSize,
+        force: this.netSettings.force,
         nodeLabels: true,
         linkLabels: true,
-      },
+      }
+    },
+  },
+  data() {
+    return {
+      netSettings: {},
     }
   },
-  async created() {
-    const bindingsStream = await this.$fetcher.getBooksByQuery()
-
-    bindingsStream.on('data', (bindings) => {
-      // book node
-      this.nodes.push({
-        id: bindings.book.value,
-        name: bindings.book.value,
-        meta: {
-          authors: bindings.authors.value,
-          publishers: bindings.publishers?.value,
-          numPages: bindings.numPages?.value,
-          isbn: bindings.isbn?.value,
-        },
-      })
-      bindings.authors.value.split(',').forEach((author) => {
-        // author node
-        this.nodes.push({
-          id: author,
-          name: author,
-          _color: 'orange',
-        })
-
-        this.links.push({
-          sid: author,
-          tid: bindings.book.value,
-        })
-      })
-    })
+  methods: {
+    resetSettings() {
+      this.$set(this.$data, 'netSettings', defaultNetSettings)
+    },
   },
 }
 </script>
--- components/Visualizator.vue
+++ components/Visualizator.vue
@@ -1,16 +1,52 @@
 <template>
-  <div class="flex flex-col md:flex-row w-full justify-between">
+  <div class="flex flex-col md:flex-row w-full justify-between max-h-screen">
     <div class="min-h-16 w-full md:w-1/3 overflow-y-auto md:mr-8">
       Tady bude vyhledavac
+      <button @click="getBooksByQuery">search</button>
     </div>
-    <NetworkCanvas class="w-full md:w-2/3 max-h-screen" />
+    <NetworkCanvas class="w-full md:w-2/3 mb-8" :nodes="nodes" :links="links" />
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {}
+    return {
+      nodes: [],
+      links: [],
+    }
+  },
+  methods: {
+    async getBooksByQuery() {
+      const bindingsStream = await this.$fetcher.getBooksByQuery()
+
+      bindingsStream.on('data', (bindings) => {
+        // book node
+        this.nodes.push({
+          id: bindings.book.value,
+          name: bindings.book.value,
+          meta: {
+            authors: bindings.authors.value,
+            publishers: bindings.publishers?.value,
+            numPages: bindings.numPages?.value,
+            isbn: bindings.isbn?.value,
+          },
+        })
+        bindings.authors.value.split(',').forEach((author) => {
+          // author node
+          this.nodes.push({
+            id: author,
+            name: author,
+            _color: 'orange',
+          })
+
+          this.links.push({
+            sid: author,
+            tid: bindings.book.value,
+          })
+        })
+      })
+    },
   },
 }
 </script>
